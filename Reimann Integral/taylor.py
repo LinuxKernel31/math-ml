68eca741bb07177501725581f44fe84bf5e12930
LinuxKernel31 <robertjohnnavas@gmail.com>
2019-12-08 01:38:03 +0800

    chore: add taylor.py

---
+++ Reimann Integral/taylor.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+__author__  = 'Robert Navas'
+__version__ = '1.0.0'
+
+
+def factorial(n):
+
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+
+
+
+
+    
