8c8b7054e608625c2a9757af986065b7b6e42c5a
LinuxKernel31 <robertjohnnavas@gmail.com>
2020-05-17 01:12:53 +0800

    chore: modify bisection.py

--- Numerical Analysis/bisection.py
+++ Numerical Analysis/bisection.py
@@ -1,25 +1,38 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__version__ = '1.0.0'
+__author__  = 'Robert Navas'
+
 import math
 
+
 def bisection(low, high, iteration):
-     root = 1e-4
-     for i in range(iteration):
+    root = 1e-4
+    for i in range(iteration):
 
-          tolerance = high - low
+        tolerance = high - low
 
-          res_midpoint = (low + high) / 2
+        res_midpoint = (low + high) / 2
 
-          result = math.pow(res_midpoint, 3) + (4 * math.pow(res_midpoint, 2)) - 10
+        result = math.pow(res_midpoint, 3) + (4 * math.pow(res_midpoint, 2)) - 10
 
-          if tolerance < root: 
-               break
-          if result > 0:
-               high = res_midpoint
-          else:
-               low = res_midpoint
+        if tolerance < root:
+            break
+        if result > 0:
+            high = res_midpoint
+        else:
+            low = res_midpoint
 
-          print('Iteration {} \t a: {:.5f} \t b: {:.5f} \t midpoint : {:.5f} \t f(p): {:.6f} \t Tolerance: :{:.5f}'.format(i+1,low,high,res_midpoint,result, tolerance))
+        print(
+            "Iteration {} \t a: {:.5f} \t b: {:.5f} \t midpoint : {:.5f} \t f(p): {:.6f} \t Tolerance: :{:.5f}".format(
+                i + 1, low, high, res_midpoint, result, tolerance
+            )
+        )
 
-     return "Bisected!"
+    return "Bisected!"
 
-if __name__ == '__main__':
-     bisection(1,2,50)
+
+if __name__ == "__main__":
+    bisection(1, 2, 50)
